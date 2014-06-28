5e1ea678528bf0df5c02dde468f6f6070c341216
Cesar Zavala <cesarzavalamesta@g.harvard.edu>
2014-06-28 17:44:45 -0400

    Initial answers added

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Cesar Zavala</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1>Cesar Zavala</h1>
+	<h2>In 2+ sentences describe who you are and what you do.</h2>
+	<p>I'm originally from Mexico, happily living in Georgia now. 
+	Having a great time in Summer school! Love playing soccer. Have 4 repos, err, I mean 4 children, so I'm totally rich but have no money :)</p>
+	<p>Professionally, I'm a Senior IT Services Analyst in <a href="http://www.burgesspigment.com">Burgess Pigment Company</a>. 
+	I do everything IT, I like development (IBM Notes) but also do systems administration, networking and everything that is related to IT.</p>
+	<h2>Why are you taking this course? </h2>
+	<p>I want to get very comfortable with Web technologies. 
+	I'm doing this as part of the <a href="http://www.extension.harvard.edu/degrees-programs/information-technology">Information Technology Graduate Program</a>.</p>
+	<h2>What previous experience do you have with HTML/CSS?</h2>
+	<p>I've created some pages and used HTML/CSS inside the IBM Notes development environment. I also took the <a href="http://www.extension.harvard.edu/courses/fundamentals-website-development">Fundamentals</a> course.</p>
+	<h2>What previous experience do you have with programming?</h2>
+	<p>I started programming a couple of years ago before my graduation from <a href="http://www.itesm.mx/wps/wcm/connect/ITESM/Tecnologico+de+Monterrey/English">Tec de Monterrey</a> back in 1996.
+	I worked with Turbo Pascal, Delphi and Visual basic before my first job.  Since then, I've worked in LotusScript ocassionally doing other stuff. </p>
+	<h2>Have you worked with PHP before? If so, to what extent?</h2>
+	<p>My only experience is inside WordPress, as part of the <a href="http://www.extension.harvard.edu/courses/introduction-web-content-management-systems-site-development">Web Content Management course</a>.</p>
+	<h2>Are you working on a Mac, PC, or some other platform?</h2>
+	<p>Mac. I've been using it since the beginning of this year, I do have a Windows VM just in case.</p>
+	<h2>Anything else we should know?</h2>
+	<p>I'm just grateful to be around these great people. Where do we sign for Susan's fan club? The whole instruction team is so talented!</p>
+</body>
+</html>
